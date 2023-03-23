f80a50d172ee534f168e8b13df512bf184d54fef
jsprofessional <jshuman@protonmail.com>
2023-03-23 17:12:33 -0500

    Got marquee working with raw css.

--- portfolio-2023/src/components/marquee-bg/MarqueeBG.tsx
+++ portfolio-2023/src/components/marquee-bg/MarqueeBG.tsx
@@ -1,28 +1,28 @@
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import "./MarqueeBG.css";
 
-const marqueeVariants = {
-  animate: {
-    x: [0, -1100],
-    transition: {
-      x: {
-        repeat: Infinity,
-        repeatType: "loop",
-        duration: 5,
-        ease: "linear",
-        delay: 0, // Add a delay to allow the text to fully exit before re-entering
-      },
-    },
-  },
-  // exit: {
-  //   x: [1200, 0], // Reverse the x values to move the text to the other side of the screen
-  //   transition: {
-  //     x: {
-  //       duration: 0, // Set duration to 0 to make the transition instant
-  //     },
-  //   },
-  // },
-};
+// const marqueeVariants = {
+//   animate: {
+//     x: [0, -1100],
+//     transition: {
+//       x: {
+//         repeat: Infinity,
+//         repeatType: "loop",
+//         duration: 5,
+//         ease: "linear",
+//         delay: 0, // Add a delay to allow the text to fully exit before re-entering
+//       },
+//     },
+//   },
+// exit: {
+//   x: [1200, 0], // Reverse the x values to move the text to the other side of the screen
+//   transition: {
+//     x: {
+//       duration: 0, // Set duration to 0 to make the transition instant
+//     },
+//   },
+// },
+// };
 
 export const MarqueeBG = () => {
   return (
@@ -37,7 +37,7 @@ export const MarqueeBG = () => {
           SEXTON <span className="my-auto">*</span>
         </h1>
       </motion.div> */}
-      <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap RightToLeft">
+      {/* <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap RightToLeft">
         ART BY JACOB SEXTON <span className="my-auto">*</span>
       </h1>
       <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap LeftToRight">
@@ -45,7 +45,75 @@ export const MarqueeBG = () => {
       </h1>
       <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap RightToLeft">
         SEXTON <span className="my-auto">*</span> ART BY JACOB
-      </h1>
+      </h1> */}
+      <div className="marquee">
+        <div className="marquee__inner" aria-hidden="true">
+          <span>
+            <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap">
+              ART BY JACOB SEXTON <span className="my-auto">*</span>
+            </h1>
+          </span>
+          <span>
+            <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap">
+              ART BY JACOB SEXTON <span className="my-auto">*</span>
+            </h1>
+          </span>
+          <span>
+            <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap">
+              ART BY JACOB SEXTON <span className="my-auto">*</span>
+            </h1>
+          </span>
+          <span>
+            <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap">
+              ART BY JACOB SEXTON <span className="my-auto">*</span>
+            </h1>
+          </span>
+        </div>
+        <div className="marquee__inner" aria-hidden="true">
+          <span>
+            <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap">
+              JACOB SEXTON <span className="my-auto">*</span> ART BY
+            </h1>
+          </span>
+          <span>
+            <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap">
+              JACOB SEXTON <span className="my-auto">*</span> ART BY
+            </h1>
+          </span>
+          <span>
+            <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap">
+              JACOB SEXTON <span className="my-auto">*</span> ART BY
+            </h1>
+          </span>
+          <span>
+            <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap">
+              JACOB SEXTON <span className="my-auto">*</span> ART BY
+            </h1>
+          </span>
+        </div>
+        <div className="marquee__inner" aria-hidden="true">
+          <span>
+            <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap">
+              SEXTON <span className="my-auto">*</span> ART BY JACOB
+            </h1>
+          </span>
+          <span>
+            <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap">
+              SEXTON <span className="my-auto">*</span> ART BY JACOB
+            </h1>
+          </span>
+          <span>
+            <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap">
+              SEXTON <span className="my-auto">*</span> ART BY JACOB
+            </h1>
+          </span>
+          <span>
+            <h1 className="black-shadow font-semibold text-white text-[220px] whitespace-nowrap">
+              SEXTON <span className="my-auto">*</span> ART BY JACOB
+            </h1>
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
